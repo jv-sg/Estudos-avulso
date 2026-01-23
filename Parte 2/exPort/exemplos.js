66b0dd2e6e6915184a6ef35018e3ecc92c5443d4
João Victor <jvictor.sg@outlook.com>
2026-01-22 23:11:51 -0300

    Atividade de criação de um sistema de cobranças automático de estacionamento

---
+++ Parte 2/exPort/exemplos.js
@@ -0,0 +1,8 @@
+
+const valorJantar = prompt("Valor do jantar R$: ")
+
+const valorGarcom = valorJantar * 0.1
+const valorTotal = Number(valorJantar) + valorGarcom
+
+alert("vALOR TOTAL: " + valorTotal)
+
